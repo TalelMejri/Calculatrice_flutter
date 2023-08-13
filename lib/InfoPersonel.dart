4dc2634a265e9bebc97091bb4a8cc3fa9e959a70
TalelMejri <talelmejri8@gmail.com>
2023-08-13 12:57:35 +0100

    add icons

--- lib/InfoPersonel.dart
+++ lib/InfoPersonel.dart
@@ -1,7 +1,6 @@
 import 'package:calculatrice/main.dart';
 import 'package:flutter/material.dart';
 
-
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
@@ -18,18 +17,35 @@ class MyWidget extends StatelessWidget {
                        )
                   ]
                  ),
-        body:SafeArea(
-            child: Container(
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                        child: const AssetImage('dsds'),
-                        backgroundColor: Colors.white,
-                    )
-                  ],
+        body:  Container(
+               margin: const EdgeInsets.only(top: 80),
+                child:   Center(
+                  child: Column(
+                    children: [
+                      const CircleAvatar(
+                        maxRadius: 100,
+                        backgroundImage: AssetImage("images/profil.jpg"),
+                       ),
+                       const SizedBox(height: 25),
+                       const Text("Talel Mejri",style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold)),
+                       const SizedBox(height: 25),
+                       Card(
+                           margin: const EdgeInsets.all(20),
+                            child: Padding(padding: const EdgeInsets.all(20),
+                             child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                               children: [
+                                  IconButton(onPressed: (){Navigator.pop(context);}, icon: Image.asset("images/github.png")),
+                                  IconButton(onPressed: (){Navigator.pop(context);}, icon:Image.asset("images/youtube.png")),
+                                  IconButton(onPressed: (){Navigator.pop(context);}, icon: const Icon(Icons.facebook))
+                               ],
+                             ),
+                            )
+                       )
+                    ],
+                  ),
                 ),
-            ),
-        )
+               )
     );
   }
 }
